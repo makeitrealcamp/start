61327de6c0600366eab4062c742568667bbd98f8
German Escobar <german.escobarc@gmail.com>
2015-03-08 14:26:52 -0500

    Fix syntax highlighting in challenge instructions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  class HTMLwithPygments < Redcarpet::Render::HTML
+  class HtmlWithPygments < Redcarpet::Render::HTML
     def block_code(code, language)
       sha = Digest::SHA1.hexdigest(code)
       Rails.cache.fetch ["code", language, sha].join('-') do
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
   
   def markdown(text)
-    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    renderer = HtmlWithPygments.new(hard_wrap: true, filter_html: true)
     options = {
       autolink: true,
       no_intra_emphasis: true,
