db887c3f7b1c27c4f17e07c6e8a647f299dc0308
German Escobar <german.escobarc@gmail.com>
2015-04-14 21:23:31 -0500

    Fix shared path for Rails evaluator

--- lib/rails_evaluator.rb
+++ lib/rails_evaluator.rb
@@ -5,7 +5,7 @@ class RailsEvaluator < Evaluator
       return
     end
 
-    dirname = "#{ENV['HOME']}/.ukku/shared/tmp/rails/user-#{solution.user_id}/solution-#{solution.id}"
+    dirname = "/tmp/ukku/rails/user-#{solution.user_id}/solution-#{solution.id}"
     FileUtils.rm_rf(dirname)
     FileUtils.mkdir_p dirname
 
@@ -13,10 +13,7 @@ class RailsEvaluator < Evaluator
     FileUtils.cp("lib/rails_template.rb", dirname)
 
     repo = "https://github.com/#{solution.repository}"
-    # container = Docker::Container.create('Image' => 'germanescobar/ruby-evaluator', 'Cmd' => ['/bin/bash', '-l', '-c', "/root/run.sh #{repo}"], 'Volumes' => { "/ukku/data" => {}, "/ukku/bundler-cache" => {} }, 'HostConfig' => { "Binds" => ["#{dirname}:/ukku/data", "#{ENV['HOME']}/.ukku/shared/bundler-cache:/ukku/bundler-cache"] })
-    # container.start
-    # status_code = container.wait(300)["StatusCode"]
-    status = Subprocess.call(["docker", "run", "-v", "#{dirname}:/ukku/data", "-v", "#{ENV['HOME']}/.ukku/shared/bundler-cache:/ukku/bundler-cache", "germanescobar/ruby-evaluator", "/bin/bash", "-c", "-l", "/root/run.sh #{repo}"])
+    status = Subprocess.call(["docker", "run", "-v", "#{dirname}:/ukku/data", "-v", "/tmp/ukku/bundler-cache:/ukku/bundler-cache", "germanescobar/ruby-evaluator", "/bin/bash", "-c", "-l", "/root/run.sh #{repo}"])
     if status.success?
       complete(solution)
     else
