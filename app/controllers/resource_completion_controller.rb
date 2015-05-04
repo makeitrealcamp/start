faa3ace1eb745b92846aff6f46f98bcd70eb7df0
German Escobar <german.escobarc@gmail.com>
2015-05-03 23:51:32 -0500

    Remove KISSmetrics

--- app/controllers/resource_completion_controller.rb
+++ app/controllers/resource_completion_controller.rb
@@ -5,7 +5,6 @@ class ResourceCompletionController < ApplicationController
     @resource = Resource.friendly.find(params[:resource_id])
     unless @resource.users.include? current_user
       @resource.users << current_user
-      KMTS.record(current_user.email, 'Completed Resource', { id: @resource.id, name: @resource.title })
     end
 
     respond_to do |format|
--- config/initializers/kissmetrics.rb
+++
@@ -1,2 +0,0 @@
-ENV['KISSMETRICS_KEY'] ||= "e15ab900b04460672c7f6e7ce1407339c8305f9d"
-KMTS.init(ENV['KISSMETRICS_KEY'], log_dir: File.join(Rails.root, 'log'))
