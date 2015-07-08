0cefe66bc4d522bfacc1a0da9d8811d653876413
nmenag <nmena.garzon@gmail.com>
2015-07-08 18:11:38 -0500

    Fix display progress

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let!(:user){ create(:user) }
+  let!(:course) { create(:course) }
   subject { User.new }
 
   context 'associations' do
@@ -41,7 +42,70 @@ RSpec.describe User, type: :model do
 
   describe '#type_student' do
     it 'return true when type is free' do
-      expect(build(:user).free_account?).to be true
+      expect(user.free_account?).to be true
+    end
+  end
+
+  describe '#completed_challenges' do
+    it 'without challenges completed' do
+      create(:challenge, course: course, published: false, restricted: true)
+      create(:challenge, course: course, published: true, restricted: true)
+      expect(user.completed_challenges(course).count).to eq 0
+    end
+
+    it 'return only the challenges completed and published'  do
+
+      challenge = create(:challenge, course: course, published: true, restricted: true)
+      challenge1 = create(:challenge, course: course, published: false, restricted: true)
+      challenge2 = create(:challenge, course: course, published: true, restricted: true)
+      #when the challenge s published does count as completed
+      create(:solution, user: user, challenge: challenge, status: :completed)
+      #when el challenge is unpublished does not count as completed
+      create(:solution, user: user, challenge: challenge1, status: :completed)
+      #when el challenge is published and the solution have status different to completed
+      #does not count as completed
+      create(:solution, user: user, challenge: challenge2, status: :created)
+      expect(user.completed_challenges(course).count).to eq 1
+    end
+  end
+
+  describe '#completed_resources' do
+    it 'without resources completed' do
+      resource  = create(:resource, course: course, published: true)
+      resource1 = create(:resource, course: course, published: false)
+      resource2 = create(:resource, course: course, published: true)
+      expect(user.completed_resources(course).count).to eq 0
+    end
+
+    it 'return only the resources completed and published' do
+      resource  = create(:resource, course: course, published: true)
+      resource1 = create(:resource, course: course, published: false)
+      resource2 = create(:resource, course: course, published: true)
+
+      create(:resource_completion, user: user, resource: resource)
+      create(:resource_completion, user: user, resource: resource1)
+      create(:resource_completion, user: user, resource: resource2)
+      expect(user.completed_resources(course).count).to eq 2
+    end
+  end
+
+  describe '#progress' do
+    it 'return 1.0 when the total is 0' do
+      expect(user.progress(course)).to eq 1.0
+    end
+
+    it 'return percentage ' do
+      challenge = create(:challenge, course: course, published: true, restricted: true)
+      challenge1 = create(:challenge, course: course, published: false, restricted: true)
+      create(:solution, user: user, challenge: challenge, status: :completed)
+
+      resource  = create(:resource, course: course, published: true)
+      resource1 = create(:resource, course: course, published: false)
+      resource2 = create(:resource, course: course, published: true)
+
+      create(:resource_completion, user: user, resource: resource)
+      create(:resource_completion, user: user, resource: resource1)
+      expect(user.progress(course)).to be < 1.0
     end
   end
 end
