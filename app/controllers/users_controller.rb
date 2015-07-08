d2bd5510a316e84ac12b52050d34271b761f3f29
nmenag <nmena.garzon@gmail.com>
2015-07-08 16:56:32 -0500

    Add password confirmation to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:email, :password, :first_name, :mobile_number, :birthday)
+      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :mobile_number, :birthday)
     end
 
     def activate_params
--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -4,7 +4,6 @@ require "rails_helper"
 RSpec.feature "Sign Up", type: :feature do
   scenario "when user is logged in" do
     login(create(:user))
-
     visit signup_path
     expect(current_path).to eq signed_in_root_path
   end
@@ -12,16 +11,19 @@ RSpec.feature "Sign Up", type: :feature do
   scenario "with valid attributes" do
     visit signup_path
     fill_in 'user_email', with: Faker::Internet.email
-    fill_in 'user_password', with: Faker::Internet.password
+    password  = Faker::Internet.password
+    fill_in 'user_password', with: password
+    fill_in 'user_password_confirmation', with: password
     click_button 'Empezar'
-
     expect(current_path).to eq signed_in_root_path
   end
 
   scenario "execute analytics script after sign up", js: true do
     visit signup_path
     fill_in 'user_email', with: Faker::Internet.email
-    fill_in 'user_password', with: Faker::Internet.password
+    password  = Faker::Internet.password
+    fill_in 'user_password', with: password
+    fill_in 'user_password_confirmation', with: password
     click_button 'Empezar'
 
     expect(current_path).to eq signed_in_root_path
@@ -48,15 +50,6 @@ RSpec.feature "Sign Up", type: :feature do
     expect(page).to have_content "Email no es válido"
   end
 
-  scenario "with no password" do
-    visit signup_path
-    fill_in 'user_email', with: 'invalid'
-    click_button 'Empezar'
-
-    expect(current_path).to eq signup_path
-    expect(page).to have_content "Email no es válido"
-  end
-
   scenario "with existing email" do
     user = create(:user)
 
@@ -67,5 +60,34 @@ RSpec.feature "Sign Up", type: :feature do
 
     expect(current_path).to eq signup_path
     expect(page).to have_content "Email ya está en uso"
+  end
+
+  scenario "with no password" do
+    visit signup_path
+    fill_in 'user_email', with: Faker::Internet.email
+    click_button 'Empezar'
+
+    expect(current_path).to eq signup_path
+    expect(page).to have_content "Password no puede estar en blanco"
+  end
+
+  scenario "with no password confirmation" do
+    visit signup_path
+    fill_in 'user_email', with: Faker::Internet.email
+    fill_in 'user_password', with: Faker::Internet.password
+    click_button 'Empezar'
+    expect(current_path).to eq signup_path
+    expect(page).to have_content "Password confirmation no puede estar en blanco"
+  end
+
+  scenario "when password  is not match" do
+    visit signup_path
+    fill_in 'user_email', with: Faker::Internet.email
+    fill_in 'user_password', with: Faker::Internet.password
+    fill_in 'user_password_confirmation', with: Faker::Internet.password
+
+    click_button 'Empezar'
+    expect(current_path).to eq signup_path
+    expect(page).to have_content "Password confirmation no coincide"
   end
 end
