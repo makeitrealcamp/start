f9377f68b07785c1992858310c8b231366a3dd80
German Escobar <german.escobarc@gmail.com>
2015-07-25 14:07:12 -0500

    Change capybara driver to selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,16 +55,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  Capybara.default_wait_time = 15
-
-  #Configuration for run tests in Chrome
-  Capybara.register_driver :chrome do |app|
-    client = Selenium::WebDriver::Remote::Http::Default.new
-    client.timeout = 120
-    Capybara::Selenium::Driver.new(app, :browser => :chrome, :http_client => client)
-  end
-
-  Capybara.javascript_driver = :chrome
+  Capybara.default_wait_time = 8
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
