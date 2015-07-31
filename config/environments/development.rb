4e536c319a64b8e98a14fea4d9a869fe2ba0a51e
Simon S. Soriano <simon0191@gmail.com>
2015-07-31 04:43:08 -0500

    configure sidekiq for development and test. Close #298

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/testing/inline'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -39,4 +40,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :letter_opener
+  config.active_job.queue_adapter = :sidekiq
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/testing/inline'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -36,6 +37,8 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.active_job.queue_adapter = :sidekiq
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
