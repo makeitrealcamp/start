2eaa2d6a2602c42a0e48a0e1c49f3ba9d0788dc5
nmenag <nmena.garzon@gmail.com>
2015-07-30 23:34:15 -0500

    Fix resource completion button

--- spec/features/resource_completition_spec.rb
+++ spec/features/resource_completition_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.feature "Resource completion", type: :feature do
   let!(:user) { create(:user) }
+  let!(:course) { create(:course) }
 
   context "when user completes a resource but there are more resources" do
     scenario "should be redirected to next resource" do
-      course = create(:course)
       resource = create(:resource, course: course)
       next_resource = create(:resource, course: course, row_position: :last)
 
@@ -19,7 +19,6 @@ RSpec.feature "Resource completion", type: :feature do
 
   context "when user completes last resource but haven`t finished course" do
     scenario "should be redirected to same course" do
-      course = create(:course)
       resource = create(:resource, course: course)
       challenge = create(:challenge, course: course)
 
@@ -58,7 +57,6 @@ RSpec.feature "Resource completion", type: :feature do
 
   context "when user completes a course but it's not the last resource", js: true do
     scenario "should be redirected to next resource" do
-      course = create(:course)
       resource = create(:resource, course: course)
       next_resource = create(:resource, course: course, row_position: :last)
       user.resource_completions.create(resource: next_resource) # user has already completed next resource
@@ -68,6 +66,35 @@ RSpec.feature "Resource completion", type: :feature do
       visit course_resource_path(course, resource)
       click_link 'Completar y Continuar'
       expect(current_path).to eq course_resource_path(course, next_resource)
+    end
+  end
+
+  context 'When they do click in link completion', js: true do
+    context 'when resource is not completed' do
+      scenario 'should mark completed' do
+        resource = create(:resource, course: course)
+        login(user)
+        visit course_path(course)
+        click_link 'Recursos'
+        all(:css, '.resource-additional .glyphicon-ok-circle').first.click
+        expect(page).to have_selector '.resource-additional .completed'
+        expect(current_path).to eq course_path(course)
+        expect(user.resources.exists?(resource.id)).to eq true
+      end
+    end
+
+    context 'when resource is completed' do
+      scenario 'should not mark completed' do
+        resource = create(:resource, course: course)
+        create(:resource_completion, resource: resource, user: user)
+        login(user)
+        visit course_path(course)
+        click_link 'Recursos'
+        all(:css, '.resource-additional .glyphicon-ok-circle').first.click
+        expect(page).not_to have_selector '.resource-additional .completed'
+        expect(current_path).to eq course_path(course)
+        expect(user.resources.exists?(resource.id)).to eq false
+      end
     end
   end
 end
