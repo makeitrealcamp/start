5d2dc09e7a2f29198722b475b7f7285286856b36
Simon S. Soriano <simon0191@gmail.com>
2015-08-26 00:34:44 -0500

    Setup figaro

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,4 +1,4 @@
 require 'pusher'
 
-Pusher.url = ENV["PUSHER_URL"] || "https://f0cdd27277f814b8b785:694e058164096f2e52ec@api.pusherapp.com/apps/136453"
+Pusher.url = ENV["PUSHER_URL"]
 Pusher.logger = Rails.logger
