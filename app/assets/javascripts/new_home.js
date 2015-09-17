2a947b07c996f983fe4b49ed8aeac109fcb1cb20
heyjoeb <heyjoeb@hotmail.com>
2015-09-17 15:37:27 -0500

    Added specific listener

--- app/assets/javascripts/new_home.js
+++ app/assets/javascripts/new_home.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  $('.scroll').click(function(){
+  $('.new_site').on('click', '.scroll', function() {
     $('html').animate({
     scrollTop: $('#anchor').offset().top - 75},
     'slow');
