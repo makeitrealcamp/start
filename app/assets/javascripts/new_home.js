f5451284fd171c9079db10576bf74536016a8826
heyjoeb <heyjoeb@hotmail.com>
2015-09-18 10:53:30 -0500

    Added more specificity to jquery listener and fixed document.ready issue with turbolinks

--- app/assets/javascripts/new_home.js
+++ app/assets/javascripts/new_home.js
@@ -1,8 +1,7 @@
-$( document ).ready(function() {
-  $('.new_site').on('click', '.scroll', function() {
+$(document).on('ready page:load', function(event) {
+  $('.home .new_site').on('click', '.scroll', function() {
     $('html').animate({
     scrollTop: $('#anchor').offset().top - 75},
     'slow');
   });
 });
-
