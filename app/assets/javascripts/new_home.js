df7684bfd888cdd8c8c5026e8cabf939e31de9bb
heyjoeb <heyjoeb@hotmail.com>
2015-09-21 15:14:39 -0500

    Fixed (again) jquery scroll issue in chrome and safari

--- app/assets/javascripts/new_home.js
+++ app/assets/javascripts/new_home.js
@@ -1,7 +1,10 @@
-$(document).on('ready page:load', function(event) {
+var ready;
+ready = function() {
   $('.home .new_site').on('click', '.scroll', function() {
-    $("html,body").animate({
+    $('html,body').animate({
     scrollTop: $('#anchor').offset().top - 75},
     'slow');
   });
-});
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
