91d28e803ec86a0bbb9136665e15990ee04ac64f
heyjoeb <heyjoeb@hotmail.com>
2015-10-20 15:50:11 -0500

    Removed phantom column ha.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930232433) do
+ActiveRecord::Schema.define(version: 20151020204644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,7 +102,6 @@ ActiveRecord::Schema.define(version: 20150930232433) do
     t.string   "slug"
     t.boolean  "published"
     t.integer  "phase_id"
-    t.integer  "phase"
   end
 
   add_index "courses", ["phase_id"], name: "index_courses_on_phase_id", using: :btree
