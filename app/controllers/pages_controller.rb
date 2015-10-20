c48c2df3b1066b79b51ab46b93cb8c5ec2a9bb1e
Simon S. Soriano <simon0191@gmail.com>
2015-10-19 23:42:27 -0500

    Refactor public pages
    - Add layout for pages controller
    - Add partial for navigation
    - Add partial for footer
    - Add partial for apply now section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,29 +2,25 @@ class PagesController < ApplicationController
   before_action :public_access, except: [:handbook]
 
   def home
-    @home = true
   end
 
   def handbook
     client = Octokit::Client.new(client_id: ENV['GITHUB_KEY'], client_secret: ENV['GITHUB_SECRET'])
     @content = client.contents("makeitrealcamp/handbook", path: "README.md", accept: "application/vnd.github.VERSION.raw").encode("ASCII-8BIT").force_encoding("utf-8")
+    render layout :application
   end
 
 
   def curriculum
-    render layout: false
   end
 
   def faq
-    render layout: false
   end
 
   def thanks
-    @home = true
   end
 
   def quotes
-    render layout: false
   end
 
 end
