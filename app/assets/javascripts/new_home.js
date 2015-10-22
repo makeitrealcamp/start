2295241b8897131b391faa2e5b0b087a06099728
heyjoeb <heyjoeb@hotmail.com>
2015-10-22 15:53:09 -0500

    Added financing options and currency switch if user is not in Colombia

--- app/assets/javascripts/new_home.js
+++ app/assets/javascripts/new_home.js
@@ -1,10 +1,23 @@
 var ready;
 ready = function() {
+  // scroll para bajar en la intro
   $('.new_site .intro').on('click', '.scroll', function() {
     $('html,body').animate({
     scrollTop: $('#anchor').offset().top - 75},
     'slow');
   });
+
+  // cambiar tipo de moneda
+  $.get("http://ipinfo.io", function(response) {
+    if(response.country == "CO") {
+        $("#currency").html("COP$2'290,000");
+        $("#financing").html("Si eliges financiación, paga 1 cuota de COP$690,000 y 6 de COP$349,000.");
+    }
+    else {
+        $("#currency").html("US$900");
+        $("#financing").html("Si eliges financiación, paga 1 cuota de US$299 y 5 de US$150.");
+    }
+  }, "jsonp"); 
 };
 $(document).ready(ready);
 $(document).on('page:load', ready);
