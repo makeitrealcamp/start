d05171d1cd1470c9e04653314a77935b6679d361
German Escobar <german.escobarc@gmail.com>
2015-10-25 00:55:02 -0500

    Implement "week progress" in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,13 @@ class DashboardController < ApplicationController
         .where('challenges.published = ?', true)
         .where('challenge_id <> ?', @challenge.try(:id))
         .shuffle.map(&:challenge).first(3)
+
+    bow = DateTime.current.beginning_of_week
+    @received_points = current_user.points.created_at_after(bow).sum(:points)
+    @solved_challenges = Challenge.published.where(id: current_user.solutions.completed_at_after(bow).pluck(:challenge_id)).count
+    @completed_resources = Resource.where(id: current_user.resource_completions.created_at_after(bow).pluck(:resource_id)).count
+    @finished_projects = Project.where(id: current_user.project_solutions.created_at_after(bow).pluck(:project_id)).count
+    @received_badges = Badge.where(id: current_user.badge_ownerships.created_at_after(bow).pluck(:badge_id)).count
   end
 
   private
