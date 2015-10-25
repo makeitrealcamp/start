b445e593d356f2f6a0e18fe1131bf5ee241dc1fc
German Escobar <german.escobarc@gmail.com>
2015-10-25 15:34:58 -0500

    Add notifications feed in dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,4 +3,8 @@ module DashboardHelper
     return 'finished' if @challenge.nil?
     @is_new_challenge ? 'start_challenge' : 'continue_challenge'
   end
+
+  def is_public_notification?(notification)
+    notification.notification_type == "level_up" || notification.notification_type == "points_earned" || notification.notification_type == "badge_earned"
+  end
 end
