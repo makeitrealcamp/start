1d70fa795d48ff6598c8707a41a52912761a9e3d
nmenag <nmena.garzon@gmail.com>
2015-10-27 18:55:09 -0500

    Add 'SLACK_TEAM' as variable enviroment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-   provider :slack  , ENV['SLACK_KEY']  , ENV['SLACK_SECRET'], scope: "identify,read,post", team: "T03KUKHBV"
+   provider :slack  , ENV['SLACK_KEY']  , ENV['SLACK_SECRET'], scope: "identify,read,post", team: ENV['SLACK_TEAM']
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
