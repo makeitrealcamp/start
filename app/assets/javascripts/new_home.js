1b8ab6d8a8c02359e9d30e8431b798126faae092
heyjoeb <heyjoeb@hotmail.com>
2015-10-27 16:43:00 -0500

    Adjusted Pricing page

--- app/assets/javascripts/new_home.js
+++ app/assets/javascripts/new_home.js
@@ -1,23 +1,11 @@
 var ready;
 ready = function() {
   // scroll para bajar en la intro
-  $('.new_site .intro').on('click', '.scroll', function() {
-    $('html,body').animate({
-    scrollTop: $('#anchor').offset().top - 75},
-    'slow');
+  $(".new_site .intro").on("click", ".scroll", function() {
+    $("html,body").animate({
+    scrollTop: $("#anchor").offset().top - 75},
+    "slow");
   });
-
-  // cambiar tipo de moneda
-  $.get("http://ipinfo.io", function(response) {
-    if(response.country == "CO") {
-        $("#currency").html("COP$2'290,000");
-        $("#financing").html("<li>" + "1 cuota inicial de COP$690,000" + " y " + "6 cuotas de COP$349,000." + "</li>");
-    }
-    else {
-        $("#currency").html("USD$900");
-        $("#financing").html("<li>" + "1 cuota inicial de USD$299" + " y " + "6 cuotas de USD$150." + "</li>");
-    }
-  }, "jsonp"); 
 };
 $(document).ready(ready);
-$(document).on('page:load', ready);
+$(document).on("page:load", ready);
