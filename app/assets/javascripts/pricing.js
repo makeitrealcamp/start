dec1320375bc16a16bac78153e8cc8588d11fd27
Simon S. Soriano <simon0191@gmail.com>
2015-10-27 18:46:42 -0500

    Refactor pricing page
    - Avoid execution of pricing script in pages different from pricing
    - Add price_box partial

--- app/assets/javascripts/pricing.js
+++ app/assets/javascripts/pricing.js
@@ -1,20 +1,21 @@
-var ready_pricing;
-ready_pricing = function() {
 
+window.ready_pricing = function() {
   // cambiar tipo de moneda
   $.get("http://ipinfo.io", function(response) {
     if(response.country == "CO") {
-      $("#financing").html("<div class='primera-cuota'><span class='price-one'>$800,000</span></br>Primera cuota</div>" + "<div class='plus'>+</div>" + "<div class='otras-cuotas'><span class='price-one'>$320,000</br></span>5 cuotas mensuales</div>" + "<p class='valor-total'>O paga <span class='directo'>$2,060,000</span> de entrada y ahorra <span class='valor-ahorro'>$340,000</span></p>" + "<div class='caption'><p>Recibimos todos los medios de pago. Precios en <strong>pesos colombianos</strong>.</p></div>");
+      $("#financing-cop").show();
     }
     else {
-      $("#financing").html("<div class='primera-cuota'><span class='price-one'>$299</span></br>Primera cuota</div>" + "<div class='plus'>+</div>" + "<div class='otras-cuotas'><span class='price-one'>$150</br></span>5 cuotas mensuales</div>" + "<p class='valor-total'>O paga <span class='directo'>$900</span> de entrada y ahorra <span class='valor-ahorro'>$149</span></p>" + "<div class='caption'><p>Recibimos todos los medios de pago. Precios en <strong>dólares americanos</strong>.</p></div>");
+      $("#financing-usd").show();
     }
-  }, "jsonp"); 
+  }).fail(function() {
+    $("#financing-cop").show();
+  });
 
   // rotar quotes
   var divs = $('div[id^="student-says-"]').hide(),
     i = 0;
-  (function cycle() { 
+  (function cycle() {
 
       divs.eq(i).fadeIn(400)
                 .delay(10000)
@@ -24,5 +25,3 @@ ready_pricing = function() {
 
   })();
 };
-$(document).ready(ready_pricing);
-$(document).on("page:load", ready_pricing);
