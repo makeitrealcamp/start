8221fa935670399d6b59e5873960c5bc882d88e4
heyjoeb <heyjoeb@hotmail.com>
2015-11-13 14:38:08 -0500

    Constructed genearl layout, added routes and adjusted main nav menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get "curriculum" => "pages#curriculum"
+  get "full_stack_web_developer" => "pages#full_stack_web_developer"
+  get "front_end_web_developer" => "pages#front_end_web_developer"
   get "pricing" => "pages#pricing"
   get "faq" => "pages#faq"
   get "makers" => "pages#makers"
