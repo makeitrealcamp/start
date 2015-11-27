8999440f4d0303ba68bf1ef71b8cb82901bd7eed
Simon S. Soriano <simon0191@gmail.com>
2015-11-27 17:55:10 -0500

    Make figaro config vars only mandatory in production

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,8 +1,10 @@
-Figaro.require_keys(
-  'PUSHER_URL',
-  'GITHUB_KEY',
-  'GITHUB_SECRET',
-  'SLACK_KEY',
-  'SLACK_SECRET',
-  'SLACK_TEAM'
-)
+if Rails.env == "production"
+  Figaro.require_keys(
+    'PUSHER_URL',
+    'GITHUB_KEY',
+    'GITHUB_SECRET',
+    'SLACK_KEY',
+    'SLACK_SECRET',
+    'SLACK_TEAM'
+  )
+end
