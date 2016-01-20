1f8c6da54f28224de849cff44eb7ea58805798eb
heyjoeb <heyjoeb@hotmail.com>
2016-01-20 10:39:43 -0500

    Improved design in login page

--- app/assets/javascripts/new_home.js
+++ app/assets/javascripts/new_home.js
@@ -6,6 +6,13 @@ ready = function() {
     scrollTop: $("#anchor").offset().top - 75},
     "slow");
   });
+
+  // agregar clase a div para poder meter imagen de background en login con slack
+  $(function() {
+    $("div.login").each(function(){
+      $(this).closest('.expand').addClass("bg-login");
+    })
+  });
 };
 $(document).ready(ready);
 $(document).on("page:load", ready);
