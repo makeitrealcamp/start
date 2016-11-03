425de214ccbbb28e8646eeb5abc26bd525cbbbc6
German Escobar <german.escobarc@gmail.com>
2016-11-03 11:28:51 -0500

    Update ConvertLoop gem

--- config/initializers/convertloop.rb
+++ config/initializers/convertloop.rb
@@ -1,5 +1,5 @@
 ConvertLoop.configure(
-  host: ENV['CONVERTLOOP_HOST'] || "http://api.convertloop.co",
+  host: ENV['CONVERTLOOP_HOST'] || "https://api.convertloop.co",
   app_id: ENV['CONVERTLOOP_APP_ID'],
   api_key: ENV['CONVERTLOOP_KEY']
 )
