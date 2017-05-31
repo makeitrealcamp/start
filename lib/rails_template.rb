d4a76c0e5b3f629a7a7389e3542333dbb127a9e2
German Escobar <german.escobarc@gmail.com>
2017-05-31 09:07:27 -0500

    Add support for Rails 5.1

--- lib/rails_template.rb
+++ lib/rails_template.rb
@@ -1,8 +1,12 @@
 gem_group :test do
   gem 'rspec-rails', '~> 3.5.2'
   gem 'shoulda-matchers', github: 'germanescobar/shoulda-matchers'
-  gem 'capybara', '~> 2.12.1'
-  gem "selenium-webdriver", '~>2.45.0'
+  if Gem::Specification.find_all_by_name("capybara").empty?
+    gem 'capybara', '~> 2.12.1'
+  end
+  if Gem::Specification.find_all_by_name("selenium-webdriver").empty?
+    gem "selenium-webdriver", '~>2.45.0'
+  end
   gem 'poltergeist'
   gem 'phantomjs', github: 'germanescobar/phantomjs-gem', require: 'phantomjs/poltergeist'
 end
