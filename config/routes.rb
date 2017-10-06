17d9fa69ac12d35f6a614e29f17f3d372ea8a076
cristian-castrillon <cristian.castrillon@gmail.com>
2017-10-05 20:07:45 -0500

    Admin: List challenges with stats

--- config/routes.rb
+++ config/routes.rb
@@ -228,6 +228,8 @@ Rails.application.routes.draw do
     resources :subjects, only:[:index] do
       patch 'update_position', on: :member
     end
+
+    resources :challenges, only: [:index]
   end
 
   resources :notifications, only: [:index,:show] do
