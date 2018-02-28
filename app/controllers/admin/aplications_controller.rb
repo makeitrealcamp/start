685f45f7a2f52c14d602565d7407c77d070895d7
David Mejia <davidndy@gmail.com>
2018-02-28 09:35:12 -0500

    Change status modal, create method for topApplicant

--- app/controllers/admin/aplications_controller.rb
+++ app/controllers/admin/aplications_controller.rb
@@ -6,4 +6,25 @@ class Admin::AplicationsController < ApplicationController
   def show
     @topApplicant = TopApplicant.find(params[:id])
   end
+
+  def create
+  	puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+  	puts params
+  	puts params[:task_id]
+  	puts params[:status]
+  	puts aplication_params	
+  	@topApplicant = TopApplicant.find(params[:task_id])
+  	@topApplicant.update(:status => params[:status])
+  	puts @topApplicant 
+
+  end
+
+  def update
+  	puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  end
+  private 
+
+  def aplication_params
+  	params.require(:top_applicant).permit(:comment )
+  end
 end
