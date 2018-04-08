933b4aebdfaffbd3c093a566b466c0e3640a5a58
Germán Escobar <german.escobarc@gmail.com>
2018-04-08 16:00:34 -0500

    Refactor top applicant integration tests

--- spec/features/admin/top_applicant_spec.rb
+++ spec/features/admin/top_applicant_spec.rb
@@ -1,78 +1,83 @@
 require 'rails_helper'
 
 RSpec.feature "top_applicant", type: :feature do
-  scenario "List and filter Top applicants", js: true do
-    user = User.create(email:"pepito@one.com",first_name:"Pepito", last_name:"gomez",password:"1234566", level_id: $2,  status: :active, account_type: :admin_account, access_type: :password)
-    top_applicant = create(:top_applicant)
-    login_password(user)
-    first("a[href$='#']").click
-    find("a[href$='/admin/top_applicants']").click
-    sleep 2
-    page.all("table.table tr").count.should == TopApplicant.count
-    query = "#{top_applicant.first_name}"
+  let(:admin) { create(:admin) }
+  let!(:applicant) { create(:top_applicant) }
+
+  scenario "admin can lists and filters top applicants", js: true do
+    login(admin)
+    visit admin_top_applicants_path
+
+    expect(page.all("table.table tr").count).to eq(TopApplicant.count)
+
+    query = applicant.first_name[0..3]
     fill_in "search-input", with: query
-    find("input#search-input").send_keys(:enter)
-    page.all("table.table tr").each do |tr|
-    tr.text[query].should == query
+    find("input#search-input").send_keys(:enter) # submit form
+
+    expect(page.all("table.table tr").count).to eq(1)
+    within :css, "table.table" do
+      expect(page).to have_content(query)
     end
   end
 
-  scenario "Allow to leave a comment in top applicant application", js: true do
-    user = User.create(email:"pepito@one.com",first_name:"Pepito", last_name:"gomez",password:"1234566", level_id: $2,  status: :active, account_type: :admin_account, access_type: :password)
-    top_applicant = create(:top_applicant)
-    login_password(user)
+  scenario "admin can add a note to an applicant", js: true do
+    login(admin)
+    visit admin_top_applicants_path
 
-    first("a[href$='#']").click
-    find("a[href$='/admin/top_applicants']").click
-    find("a[href$='/admin/top_applicants/1']").click
+    find("#applicant-#{applicant.id} .cell-action a").click
+
     fill_in "note_applicant_activity_body", with: "Esta es la primera nota del día"
-    find("input[type='submit']").click
-    sleep 2
-    applicant = TopApplicant.find(top_applicant.id)
+    click_on "Agregar Nota"
+
+    within :css, ".activities" do
+      expect(page).to have_content("Esta es la primera nota del día")
+    end
+
+    applicant.reload
     expect(applicant.note_activities.count).to eq(1)
-    sleep 2
   end
 
-  scenario "Allow to change status in top applicant application", js: true do
-    user = User.create(email:"pepito@one.com",first_name:"Pepito", last_name:"gomez",password:"1234566", level_id: $2,  status: :active, account_type: :admin_account, access_type: :password)
-    top_applicant = create(:top_applicant)
-    login_password(user)
-    first("a[href$='#']").click
-    find("a[href$='/admin/top_applicants']").click
-    find("a[href$='/admin/top_applicants/1']").click
-    find("a[href$='/admin/top_applicants/1/change_status_applicant_activities/new']").click
-    select "Aceptado", :from => "change_status_applicant_activity_to_status"
-    fill_in "change_status_applicant_activity_comment", with:"Status changed to test send"
-    find("input[type='submit']").click
-    sleep 2
-    expect(TopApplicant.find(top_applicant.id).status).to eq("accepted")
+  scenario "admin can change the status of an applicant", js: true do
+    login(admin)
+    visit admin_top_applicants_path
+
+    find("#applicant-#{applicant.id} .cell-action a").click
+
+    click_on "Cambiar Estado"
+    select "Aceptado", from: "change_status_applicant_activity_to_status"
+    fill_in "change_status_applicant_activity_comment", with: "Status changed to test send"
+    within :css, "#change-status-modal" do
+      click_on "Cambiar Estado"
+    end
+    expect(page).to have_no_css("#change-status-modal")
+
+    expect(TopApplicant.find(applicant.id).status).to eq("accepted")
   end
 
-  scenario "Allow to send an email in top applicant application", js: true do
-    ActionMailer::Base.deliveries.clear
-    user = User.create(email:"pepito@one.com",first_name:"Pepito", last_name:"gomez",password:"1234566", level_id: $2,  status: :active, account_type: :admin_account, access_type: :password)
-    top_applicant = create(:top_applicant)
-    login_password(user)
-    first("a[href$='#']").click
-    find("a[href$='/admin/top_applicants']").click
-    find("a[href$='/admin/email_templates']").click
-    find("a[href$='/admin/email_templates/new']").click
-    fill_in "email_template_title", with: "Plantilla 1 "
-    sleep 1
-    fill_in "email_template_subject", with: "Citado a entrevista de inglés "
-    sleep 1
-    fill_in "email_template_body", with: "Cita con Juan Gomez el día viernes 9:00 COL"
-    find("input[type='submit'").click
-    expect(find("table.table tbody tr")).not_to be_nil
-    first("a[href$='#']").click
-    find("a[href$='/admin/top_applicants']").click
-    find("a[href$='/admin/top_applicants/1']").click
-    find("a[href$='/admin/top_applicants/1/email_applicant_activities/new']").click
-    sleep 2
-    select "Plantilla 1", :from => "email_template"
-    sleep 2
-    find("input[type='submit']").click
-    sleep 2
+  scenario "admin can send an email to an applicant", js: true do
+    login(admin)
+    visit admin_email_templates_path
+
+    click_on "Nueva Plantilla"
+
+    expect {
+      fill_in "email_template_title", with: "Plantilla 1"
+      fill_in "email_template_subject", with: "Citado a entrevista de inglés"
+      fill_in "email_template_body", with: "Cita con Juan Gomez el día viernes 9:00 COL"
+      click_on "Guardar Plantilla"
+      expect(page).to have_no_css("#email-template-modal")
+    }.to change(EmailTemplate, :count).by 1
+
+    visit admin_top_applicant_path(applicant)
+    click_on "Enviar Email"
+    expect(page).to have_css("#send-email-modal")
+
+    sleep 0.5 # hack to wait for the animation
+    select "Plantilla 1", from: "email_template"
+    # expect(page).to have_content("Citado a entrevista de inglés")
+    click_on "Enviar"
+    expect(page).to have_no_css("#send-email-modal")
+
     email = ActionMailer::Base.deliveries
     expect(email.count).to eq(1)
   end
