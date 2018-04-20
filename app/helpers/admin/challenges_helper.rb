12347fa974937aa79a18a9c899299b8460973751
Germán Escobar <german.escobarc@gmail.com>
2018-04-20 12:42:46 -0500

    Refactor admin challenges

--- app/helpers/admin/challenges_helper.rb
+++ app/helpers/admin/challenges_helper.rb
@@ -1,25 +1,19 @@
 module Admin::ChallengesHelper
-  def percentage(challenge)
-    if challenge.solutions.where("attempts = 1").count == 0
-      percentage = challenge.solutions.where("attempts = 1").count
-    else
-      percentage = (challenge.solutions.where("status > 0").count*100)/challenge.solutions.where("attempts = 1").count
-    end
-      if percentage < 80
-        concat content_tag(:span, "#{percentage}% (#{challenge.solutions.where("status > 0").count})", class: "challenge-effectiveness-red")
-      else
-        concat content_tag(:span, "#{percentage}% (#{challenge.solutions.where("status > 0").count})", class: "challenge-effectiveness")
-      end
-      nil
+  def challenge_effectiveness(challenge)
+    completed = challenge.solutions.where(status: Solution.statuses[:completed]).count
+    attempted = challenge.solutions.where("attempts > ?", 1).count
+
+    percentage = attempted > 0 ? (completed / attempted.to_f) * 100 : 0
+    content_tag :span, "#{number_to_percentage(percentage, precision: 0)} (#{completed})",
+        class: percentage < 80 ? "critical" : ""
   end
 
 
-  def attempts(challenge)
-    if challenge.solutions.average("attempts") > 5.0
-      concat content_tag(:span, challenge.solutions.average("attempts"), class: "challenge-average-red")
-    else
-      concat content_tag(:span, challenge.solutions.average("attempts"), class: "challenge-average")
-    end
-    nil
+  def challenge_attempts_average(challenge)
+    value = challenge.solutions.average(:attempts)
+    value = 0 if value.nil?
+
+    content_tag :span, "#{number_with_precision(value, precision: 1, locale: :en)}",
+        class: value > 5 ? "critical" : ""
   end
 end
