5e9477d8fed0523c74feff5d6b42a090515637e4
Germán Escobar <german.escobarc@gmail.com>
2018-10-08 15:13:11 -0500

    Add the React and Redux Medellin course

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   get "elite", to: "pages#elite"
   get "courses/react-redux", to: "pages#react_redux"
   get "courses/nodejs-mongodb/medellin", to: "pages#nodejs_medellin"
+  get "courses/react-redux/medellin", to: "pages#react_medellin"
 
   post "application/top", to: "pages#create_top_applicant"
 
