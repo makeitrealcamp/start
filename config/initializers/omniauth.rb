f6c5d63081a705b765d0fea114ac9be550ba86bb
Germán Escobar <german.escobarc@gmail.com>
2019-06-13 09:55:02 -0500

    Change Slack scopes for OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
    provider :slack, ENV['SLACK_KEY'], ENV['SLACK_SECRET'],
-    scope: "identify,team:read,users:read,users:read.email",
+    scope: "identify",
     team: ENV['SLACK_TEAM']
 end
 
