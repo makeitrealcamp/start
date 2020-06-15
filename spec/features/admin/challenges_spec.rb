87143096c6a07154faacd3a92d3b7aaa6031e7d8
Germán <german.escobarc@gmail.com>
2020-06-15 01:44:50 -0500

    Admin: Fix update challenge

---
+++ spec/features/admin/challenges_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Challenges management", type: :feature do
+  let(:admin) { create(:admin) }
+  let(:subject) { create(:subject) }
+  let(:challenge) { create(:challenge, subject: subject) }
+
+  scenario "updates a challenge" do
+    login(admin)
+    visit edit_admin_subject_challenge_path(subject, challenge)
+
+    click_button 'Editar Reto'
+    expect(current_path).to eq subject_challenge_path(subject, challenge)
+  end
+end
