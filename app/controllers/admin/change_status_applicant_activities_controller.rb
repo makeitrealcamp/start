6d4c8338ea549c1ca1efb21d52bf48a29e265f98
Germán Escobar <german.escobarc@gmail.com>
2021-12-08 23:00:29 -0500

    Fix applicant status change

--- app/controllers/admin/change_status_applicant_activities_controller.rb
+++ app/controllers/admin/change_status_applicant_activities_controller.rb
@@ -8,9 +8,10 @@ class Admin::ChangeStatusApplicantActivitiesController < ApplicationController
 
   def create
     applicant = Applicant.find(params[:applicant_id])
+    from_status = applicant.status
     applicant.update(status: activity_params[:to_status])
 
-    data = { user: current_user, from_status: applicant.status }
+    data = { user: current_user, from_status: from_status }
     @activity = applicant.change_status_activities.create(activity_params.merge(data))
   end
 
