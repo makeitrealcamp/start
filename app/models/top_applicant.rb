73bee7364d7331c1d6c1f83642fb0fc2f1821418
Leonel Oliveros <leoneloliveros.co@gmail.com>
2022-04-21 14:06:06 -0500

    Admin: Adding status on enum of TopApplicant

--- app/models/top_applicant.rb
+++ app/models/top_applicant.rb
@@ -21,7 +21,7 @@
 #
 
 class TopApplicant < Applicant
-  enum status: [:applied, :test_sent, :test_received, :test_graded, :first_interview_scheduled, :second_interview_held, :accepted, :enrolled, :not_enrolled, :rejected, :interviews_completed, :gave_up]
+  enum status: [:applied, :test_sent, :test_received, :test_graded, :first_interview_scheduled, :second_interview_held, :accepted, :enrolled, :not_enrolled, :rejected, :interviews_completed, :gave_up, :graduated, :placed]
 
   before_create :generate_uid
 
