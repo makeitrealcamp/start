3e5259bc808ea0a046e9ca62776ae43423d497ed
Leonel Oliveros <leoneloliveros.co@gmail.com>
2022-06-08 15:26:15 -0500

    Admin: adding substatus for second interview

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -40,10 +40,18 @@ class Applicant < ApplicationRecord
     results
   end
 
+  def self.info_fields_substatus(status)
+    mappings = {
+      rejected: 'rejected_reason',
+      second_interview_held: 'second_interview_substate'
+    }
+    mappings[status.to_sym]
+  end
+
   def self.find_applicant_by_substate(status, substate)
     results = all.where(status: TopApplicant.statuses[status])
     results = results.joins('left join applicant_activities t on t.applicant_id = applicants.id AND t.id = (SELECT MAX(id) FROM applicant_activities WHERE applicant_activities.applicant_id = t.applicant_id)')
-    .where("t.info -> 'rejected_reason' = ?", substate)
+    .where("t.info -> '#{self.info_fields_substatus(status)}' = ?", substate)
     results
   end
 end
