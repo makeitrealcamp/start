b617c5f30005e2f5ebb33f6c9ceadf92abd16f84
Germán Escobar <german.escobarc@gmail.com>
2022-07-25 06:59:44 -0500

    Create Nominapp dashboard

---
+++ app/models/group.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Group < ApplicationRecord
+  has_many :users
+end
