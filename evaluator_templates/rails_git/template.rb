efc06b57b4c6295b6ccbe32c5f4088e4e77d6750
Germán Escobar <german.escobarc@gmail.com>
2022-09-09 09:38:33 -0500

    Fix another issue with the Rails executor

--- evaluator_templates/rails_git/template.rb
+++ evaluator_templates/rails_git/template.rb
@@ -1,6 +1,9 @@
 gem_group :test do
   gem 'rspec-rails', '~> 4.1.2'
   gem 'shoulda-matchers', '3.1.3'
+  gem 'net-smtp', require: false
+  gem 'net-imap', require: false
+  gem 'net-pop', require: false
   if Gem::Specification.find_all_by_name("capybara").empty?
     gem 'capybara', '~> 2.12.1'
   end
