82f15f8d702d330b39897691997281230a80b8ab
Leonel Oliveros <leoneloliveros.co@gmail.com>
2022-10-28 17:09:16 -0500

    removing legacy code and adding new validation of status

--- app/controllers/billing/stripe_controller.rb
+++ app/controllers/billing/stripe_controller.rb
@@ -2,12 +2,23 @@ class Billing::StripeController < ApplicationController
   def success
     @stripe_transaction = Stripe::Checkout::Session.retrieve(params[:session_id])
     @charge = Billing::Charge.find(@stripe_transaction.metadata.chargeId)
-    @charge.status =  @stripe_transaction.payment_status.to_sym
-    render "billing/charges/stripe/show", layout: "pages"
+    update_status(@charge, @stripe_transaction.payment_status)
+    
+    render "billing/charges/show", layout: "pages"
   end
 
   def cancel
     redirect_to full_stack_online_cupo_path( nil, currency: 'USD')
+  end
+
+  private
+
+  def update_status(charge, status)
+    if status == "paid"
+      charge.paid!
+    elsif status == "unpaid"
+      charge.pending!
+    end
   end
 
 end
