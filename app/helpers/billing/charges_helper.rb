4225e51cec975454872579b06e0e6ac804e9e54f
Leonel Oliveros <leoneloliveros.co@gmail.com>
2022-10-28 17:09:16 -0500

    removing legacy code and adding new validation of status

--- app/helpers/billing/charges_helper.rb
+++ app/helpers/billing/charges_helper.rb
@@ -1,6 +1,6 @@
 module Billing::ChargesHelper
   def charge_header_tag(charge)
-    class_name, title = if (charge == "complete" || charge.paid?)
+    class_name, title = if charge.paid?
       ["success", "TRANSACCIÓN EXITOSA"]
     elsif charge.created? || charge.pending?
       ["info", "PENDIENTE POR CONFIRMAR"]
@@ -11,19 +11,6 @@ module Billing::ChargesHelper
     end
     
     content_tag 'div', "<strong>#{title}</strong>".html_safe, class: "alert alert-#{class_name}"
-  end
-
-  def charge_stripe_status_tag(charge)
-    class_name, title = if charge == "complete"
-      ["success", "TRANSACCIÓN EXITOSA"]
-    elsif charge == "incomplete"
-      ["info", "PENDIENTE POR CONFIRMAR"]
-    elsif charge == "incomplete_expired"
-      ["danger", "TRANSACCIÓN EXPIRADA"]
-    elsif charge == "canceled"
-      ["danger", "TRANSACCIÓN CANCELADA"]
-    end
-    content_tag 'div', "<strong>#{title}</strong>".html_safe, class: "alert alert-#{class_name}" 
   end
 
 end
