780d1c56d34d10fd726fc91c6f939ba8c05ef7a3
Leonel Oliveros <leoneloliveros.co@gmail.com>
2022-10-31 09:19:31 -0500

    adding error message to transaction

--- app/controllers/billing/stripe_controller.rb
+++ app/controllers/billing/stripe_controller.rb
@@ -1,9 +1,10 @@
 class Billing::StripeController < ApplicationController
   def success
     @stripe_transaction = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_result = Stripe::PaymentIntent.retrieve(@stripe_transaction.payment_intent)
     @charge = Billing::Charge.find(@stripe_transaction.metadata.chargeId)
-    update_status(@charge, @stripe_transaction.payment_status)
-    
+    update_status(@charge, @payment_result)
+
     render "billing/charges/show", layout: "pages"
   end
 
@@ -13,11 +14,12 @@ class Billing::StripeController < ApplicationController
 
   private
 
-  def update_status(charge, status)
-    if status == "paid"
+  def update_status(charge, payment)
+    if payment.status == "succeeded"
       charge.paid!
-    elsif status == "unpaid"
-      charge.pending!
+    elsif payment.status == "payment_failed"
+      charge.rejected!
+      charge.error_message = payment.last_payment_error.message
     end
   end
 
