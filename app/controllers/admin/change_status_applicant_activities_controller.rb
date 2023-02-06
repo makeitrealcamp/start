9802d5aa2b2cc71d27a09f55b89561fa5b5c703c
Leonel Oliveros <leoneloliveros.co@gmail.com>
2023-02-06 11:34:24 -0500

    refactoring converloop

--- app/controllers/admin/change_status_applicant_activities_controller.rb
+++ app/controllers/admin/change_status_applicant_activities_controller.rb
@@ -11,17 +11,11 @@ class Admin::ChangeStatusApplicantActivitiesController < ApplicationController
     from_status = applicant.status
     applicant.update(status: activity_params[:to_status])
     if applicant.type == 'TopApplicant'
-      ConvertLoop.event_logs.send(
-        name: applicant.class.status_segments(activity_params[:to_status]),
-        person: {
-          email: applicant.email
-        },
-        metadata: { 
-          rejection_reason: activity_params[:rejected_reason],
-          second_interview_substate: activity_params[:second_interview_substate]
-        }
-      )
-      
+      metadata = {}
+      if activity_params[:to_status] == 'rejected'
+        metadata[:rejection_reason] = activity_params[:rejected_reason]
+      end
+      notify_convertloop(applicant.class.status_segments(activity_params[:to_status].to_sym), applicant, metadata)
     end
 
     data = { user: current_user, from_status: from_status }
@@ -31,5 +25,17 @@ class Admin::ChangeStatusApplicantActivitiesController < ApplicationController
   private
     def activity_params
       params.require(:change_status_applicant_activity).permit(:to_status, :comment, :rejected_reason, :second_interview_substate)
+    end
+
+    def notify_convertloop(event, applicant, metadata)
+      ConvertLoop.event_logs.send(
+        name: event,
+        person: {
+          email: applicant.email
+        },
+        metadata: metadata
+      )
+    rescue => e
+      Rails.logger.error "Couldn't send event to ConvertLoop: #{e.message}"
     end
 end
