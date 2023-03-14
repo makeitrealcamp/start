2cf36aa6470b5713b9556169d1c9eff9a9ab3ff7
Germán Escobar <german.escobarc@gmail.com>
2023-03-13 23:30:41 -0500

    Update root route

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,18 +3,6 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   render_views
 
-  describe "GET #home" do
-    it "responds with status code 200" do
-      get :home
-      expect(response).to have_http_status(:ok)
-    end
-
-    it "renders template" do
-      get :home
-      expect(response).to render_template :home
-    end
-  end
-
   describe "GET #top" do
     it "responds with status code 200" do
       get :top_full_and_part_time
